7e0b0ac5560c601ccb0eca431fcbacd3c6d0937e
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-07-30 23:06:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
+// Convert the input string to lowercase
+  str = str.toLowerCase();
 
+  // Remove all non-alphanumeric characters from the string
+  str = str.replace(/[^a-z0-9]/g, '');
+
+  // Compare the original string with its reverse
+  return str === str.split('').reverse().join('');
 }
+console.log(palindrome("racecar")); // Output: true
+console.log(palindrome("RaceCar")); // Output: true
+console.log(palindrome("raceCAR")); // Output: true
 module.exports = palindrome
